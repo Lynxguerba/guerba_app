29fc5722de6d70702208cbfd599d8dc562fcc3ba
GU15 <119710589+Lynxguerba@users.noreply.github.com>
2024-10-29 17:04:37 +0800

    commit

--- lib/project.dart
+++ lib/project.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
+// ignore_for_file: prefer_const_constructors, no_leading_underscores_for_local_identifiers, , prefer_const_literals_to_create_immutables, prefer_const_literals_to_create_immutables
+// prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:guerba_app/aboutme.dart';
@@ -6,20 +7,61 @@ import 'package:guerba_app/chiper.dart';
 import 'package:guerba_app/login_page.dart';
 import 'package:guerba_app/welcome_page.dart';
 
-class Projects extends StatelessWidget {
+class Projects extends StatefulWidget {
   const Projects({super.key});
-  
-  get accountName => 'dddd';
+
+  @override
+  _ProjectsState createState() => _ProjectsState();
+}
+
+class _ProjectsState extends State<Projects> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page!.round();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildPageIndicator(int pageCount) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(pageCount, (index) {
+        return AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          margin: EdgeInsets.symmetric(horizontal: 4.0),
+          height: 8.0,
+          width: _currentPage == index ? 12.0 : 8.0,
+          decoration: BoxDecoration(
+            color: _currentPage == index ? Colors.white : Colors.grey,
+            shape: BoxShape.circle,
+          ),
+        );
+      }),
+    );  }
 
   @override
   Widget build(BuildContext context) {
+    final int _numPages = 3;
+
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 6, 0, 30),
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 6, 0, 30),
         iconTheme: IconThemeData(
-          color: Colors.white,
-        ),
+          color: Colors.white
+          ),
       ),
       drawer: Drawer(
         backgroundColor: Colors.grey[300],
@@ -30,43 +72,34 @@ class Projects extends StatelessWidget {
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/images/navbar-image.jpg'),
-                  fit: BoxFit
-                      .cover, // This makes the image cover the whole header
+                  fit: BoxFit.cover,
                 ),
               ),
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 30),
-                    Image.asset(
-                      'assets/images/chatbot.jpg',
-                      width: 80,
-                      height: 80,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 30),
+                  Image.asset(
+                    'assets/images/chatbot.jpg',
+                    width: 80,
+                    height: 80,
+                  ),
+                  Flexible(
+                    child: Text(
+                      'GuerbsWorld',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    Flexible(
-                      child: Text(
-                        'GuerbsWorld',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             ListTile(
               leading: Icon(Icons.home),
               title: Text('Home'),
               onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            WelcomePage(accountName: accountName)));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WelcomePage(accountName: '',)));
               },
             ),
             ListTile(
@@ -83,11 +116,8 @@ class Projects extends StatelessWidget {
               leading: Icon(Icons.person),
               title: Text('About Me'),
               onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            AboutMe()));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AboutMe()));
               },
             ),
             ListTile(
@@ -116,6 +146,30 @@ class Projects extends StatelessWidget {
             ),
           ],
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              children: [
+                Center(
+                    child: Text('Page 1',
+                        style: TextStyle(color: Colors.white, fontSize: 24))),
+                Center(
+                    child: Text('Page 2',
+                        style: TextStyle(color: Colors.white, fontSize: 24))),
+                Center(
+                    child: Text('Page 3',
+                        style: TextStyle(color: Colors.white, fontSize: 24))),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: _buildPageIndicator(_numPages),
+          ),
+        ],
       ),
     );
   }
